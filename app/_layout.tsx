8227bbe847b323ecf6956a1db35d9580e22003bd
Vishwam Dhavale <vishwamdhavale@gmail.com>
2025-06-24 19:22:48 +0530

    refactor: wrap Tabs component in SafeAreaView for improved layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,13 @@
 import { Tabs } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
-      <Tabs.Screen name="forms" options={{ title: "Forms" }} />
-    </Tabs>
+    <SafeAreaView style={{ flex: 1 }} edges={["bottom"]}>
+      <Tabs screenOptions={{ headerShown: false, tabBarActiveTintColor: "#673ab7", tabBarInactiveTintColor: "#999", tabBarStyle: { backgroundColor: "#fff" } }}>
+        <Tabs.Screen name="index" options={{ title: "Home" }}  />
+        <Tabs.Screen name="forms" options={{ title: "Forms" }} />
+      </Tabs>
+    </SafeAreaView>
   );
 }
